7b51d3adf031f36a184638d3ac4d9a3af7223e65
Shweta <Shweta@gmail.com>
2025-12-18 17:20:12 -0500

    worked on operators today

---
+++ Python/operators.py
@@ -0,0 +1,25 @@
+a, b = 6, 10
+#below are arithimatic operators 
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
+print("a//b =", a//b)
+print("a%b =", a%b)
+print("a**b =", a**b)
+#below are comparision operators
+print("a==b =", a==b)
+print("a!=b =", a!=b)
+print("a>b =", a>b)
+print("a<b =", a<b)
+print("a>=b =", a>=b)
+print("a<=b =", a<=b)
+#assignment operators
+print("a+=5 will make a=", a+5)
+print("b+=5 will make =", b+5)
+print("a=", a)
+a+=5 # it means a = a+5
+print("a now is =", a)
+print("b=", b)
+b = b+5
+print("b now is =", b)
